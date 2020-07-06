63242cb4c7a816ef971a4b9548fd4fe65c32d4d1
dhanishtaghosh <dhanishthaghosh@gmail.com>
2020-07-07 03:35:33 +0530

    User account

--- booksbuddies/forms.py
+++ booksbuddies/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm, RecaptchaField
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from booksbuddies.models import User
@@ -64,5 +66,39 @@ class SellForm(FlaskForm):
     subject = StringField('Subject', 
         validators=[DataRequired()])
     semester = SelectField('Semester', validators=[DataRequired()],
-        choices=[('sem1', 'Semester-I'), ('sem2', 'Semester-II'), ('sem3', 'Semester-III'), ('sem4', 'Semester-IV'), ('sem5', 'Semester-V'), ('sem6', 'Semester-VI'), ('sem7', 'Semester-VII'), ('sem8', 'Semester-VIII')])
+        choices=[('sem1', 'Semester-I'), ('sem2', 'Semester-II')])
     submit = SubmitField('Upload Book Details') 
+
+class UpdateAccountForm(FlaskForm):
+    firstname = StringField('First Name',
+                           validators=[DataRequired(), Length(min=1, max=20)])
+    lastname = StringField('Last Name',
+                           validators=[DataRequired(), Length(min=1, max=20)])
+    id_num = StringField('Identity Number',
+                            validators=[DataRequired(), Length(min=9, max=9)])
+    branch = SelectField('Branch Name', 
+                        choices=[('none', 'None'), ('civil', 'Civil Enginnering'), ('comps', 'Computer Science'), ('elec', 'Electrical Engineering'), ('tronic', 'Electronics Engineering'), ('extc', 'Electronics and Telecommunication Enginnering'), ('it', 'Information Technology'), ('mech', 'Mechanical Engineering'), ('prod', 'Production Engineering'), ('text', 'Textile Engineering')])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update Information')
+
+    def validate_id_num(self, id_num):
+        if id_num.data != current_user.id_num:
+            user = User.query.filter_by(id_num=id_num.data).first()
+            if user:
+                raise ValidationError('This ID number is alreedy in use. Please choose a different one.')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This email is taken. Please choose a different one.')
--- booksbuddies/routes.py
+++ booksbuddies/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image 
 from flask import render_template, url_for, flash, redirect, request 
 from booksbuddies import app, db, bcrypt
-from booksbuddies.forms import RegistrationForm, LoginForm
+from booksbuddies.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from booksbuddies.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -15,12 +18,15 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    # print('Entered register method')
     form = RegistrationForm()
     if form.validate_on_submit():
+        # print("Registration Data submitted")
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(firstname=form.firstname.data, lastname=form.lastname.data, id_num=form.id_num.data, branch=form.branch.data, username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
+        # print(firstname, lastname, id_num, branch, username, email) 
         flash('Account created for {form.username.data}! From next time onwards you can login.', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
@@ -45,7 +51,56 @@ def logout():
     logout_user()
     return redirect(url_for('home')) 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/pictures', picture_fn)
+
+    output_size = (130, 130)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account') 
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.firstname = form.firstname.data
+        current_user.lastname = form.lastname.data
+        current_user.id_num = form.id_num.data
+        current_user.branch = form.branch.data 
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.firstname.data = current_user.firstname
+        form.lastname.data = current_user.lastname
+        form.id_num.data = current_user.id_num
+        form.branch.data = current_user.branch
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='pictures/' + current_user.image_file)
+    return render_template('account.html', title='My Account',
+                           image_file=image_file, form=form) 
+
+@app.route('/buy') 
+def buy():
+    return render_template('buy.html', title='Buy') 
+
+@app.route('/buy/based on subject')
+def buy2_1():
+    return render_template('buy2_1.html', title='Buy | Based on Subject') 
+
+@app.route('/sell', methods=['GET', 'POST']) 
+@login_required
+def sell():
+    return render_template('sell.html', title='Sell') 
