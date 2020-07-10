de8cf01c70753883ce6ce3f848780b51b890f97b
dhanishtaghosh <dhanishthaghosh@gmail.com>
2020-07-10 23:37:50 +0530

    Removed flask_migrate dependency

--- booksbuddies/__init__.py
+++ booksbuddies/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_migrate import Migrate
 
 
 app = Flask (__name__,
@@ -14,7 +13,6 @@ app = Flask (__name__,
 app.config['SECRET_KEY'] = '91d54e9262c3491e8a22b74c2d0e2bca'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-migrate = Migrate(app) 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
