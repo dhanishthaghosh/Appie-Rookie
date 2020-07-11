665babd463a4915978542aa41de78d0fb06c20c1
dhanishtaghosh <dhanishthaghosh@gmail.com>
2020-07-12 01:26:49 +0530

    Time reduced

--- booksbuddies/models.py
+++ booksbuddies/models.py
@@ -20,7 +20,7 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.png')
     books = db.relationship('Book', backref='owner', lazy=True)
 
-    def get_reset_token(self, expires_sec=1200):
+    def get_reset_token(self, expires_sec=600):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
